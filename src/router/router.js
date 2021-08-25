80a9b9bb37b0aab354dd6de5a607044b73e3f691
steeltx <realsteel.gnu@gmail.com>
2021-08-25 00:46:07 -0500

    Agregando pagina de 404

--- src/router/router.js
+++ src/router/router.js
@@ -3,13 +3,15 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import ListPage from '../modules/pokemon/pages/ListPage.vue'
 import AboutPage from '../modules/pokemon/pages/AboutPage.vue'
 import PokemonPage from '../modules/pokemon/pages/PokemonPage.vue'
+import NoPageFound from '../modules/shared/pages/NoPageFound.vue'
 
 
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
-  ]
+    { path: '/:pathMach(.*)*', component: NoPageFound}
+]
 
   const router = createRouter({
     history: createWebHashHistory(),
