61cf071d0b5f95bd313589f9e724cf6e3c305fd7
steeltx <realsteel.gnu@gmail.com>
2021-08-25 23:03:53 -0500

    Implementando LazyLoad

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,18 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
-import ListPage from '../modules/pokemon/pages/ListPage.vue'
-import AboutPage from '../modules/pokemon/pages/AboutPage.vue'
-import PokemonPage from '../modules/pokemon/pages/PokemonPage.vue'
-import NoPageFound from '../modules/shared/pages/NoPageFound.vue'
-
-
 const routes = [
-    { path: '/', component: ListPage },
-    { path: '/about', component: AboutPage },
-    { path: '/id', component: PokemonPage },
-    { path: '/:pathMach(.*)*', component: NoPageFound}
+    { 
+      path: '/', component: () => import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage') 
+    },
+    { 
+      path: '/about', component: () => import(/*webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage') 
+    },
+    { 
+      path: '/id', component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage')  
+    },
+    { 
+      path: '/:pathMach(.*)*', component:  () => import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')  
+    }
 ]
 
   const router = createRouter({
