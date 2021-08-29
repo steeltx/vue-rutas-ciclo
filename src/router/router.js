57668cd7897d1f81bc1c070afd01e084f6ab8857
steeltx <realsteel.gnu@gmail.com>
2021-08-29 15:18:56 -0500

    Creando routerLink personalizado

--- src/router/router.js
+++ src/router/router.js
@@ -1,28 +1,38 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
 const routes = [
-    { 
-      path: '/', component: () => import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage') 
+    {
+      path:'/',
+      redirect: '/home'
     },
     { 
-      path: '/about', component: () => import(/*webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage') 
+      path: '/home', 
+      name:'home',
+      component: () => import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage') 
     },
     { 
-      path: '/:id', component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage'),
+      path: '/about', 
+      name:'about',
+      component: () => import(/*webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage') 
+    },
+    { 
+      path: '/pokemonid/:id', 
       name: 'pokemon-id',
+      component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage'),
       props:(route) => {
         const id = Number(route.params.id)
         return isNaN( id ) ? { id: 1 } : { id }
       }
     },
     { 
-      path: '/:pathMach(.*)*', component:  () => import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')  
+      path: '/:pathMach(.*)*', 
+      component:  () => import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')  
     }
 ]
 
-  const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-  })
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
 
-  export default router
+export default router
