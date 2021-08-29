625587fefebab63380cf21e8960493926c796421
steeltx <realsteel.gnu@gmail.com>
2021-08-29 18:22:01 -0500

    Rutas hijas para DBZ

---
+++ src/modules/dbz/layouts/DragonBallLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dragolball-layout">
+    <h1>DBZ layout</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+    .dragolball-layout{
+        background-color: deepskyblue;
+    }
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,44 +1,65 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-    {
-      path:'/',
-      redirect: '/pokemon'
-    },
-    {
-      path:'/pokemon',
-      name:'pokemon',
-      component: () => import(/*webpackChunkName: "PokemonLayout"*/'../modules/pokemon/layouts/PokemonLayout'),
-      children: [
-        { 
-          path: 'home', 
-          name:'pokemon-home',
-          component: () => import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage') 
+  {
+    path: '/',
+    redirect: '/pokemon'
+  },
+  {
+    path: '/pokemon',
+    name: 'pokemon',
+    component: () => import(/*webpackChunkName: "PokemonLayout"*/'../modules/pokemon/layouts/PokemonLayout'),
+    children: [
+      {
+        path: 'home',
+        name: 'pokemon-home',
+        component: () => import(/*webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage')
+      },
+      {
+        path: 'about',
+        name: 'pokemon-about',
+        component: () => import(/*webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage')
+      },
+      {
+        path: 'pokemonid/:id',
+        name: 'pokemon-id',
+        component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage'),
+        props: (route) => {
+          const id = Number(route.params.id)
+          return isNaN(id) ? { id: 1 } : { id }
         },
-        { 
-          path: 'about', 
-          name:'pokemon-about',
-          component: () => import(/*webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage') 
-        },
-        { 
-          path: 'pokemonid/:id', 
-          name: 'pokemon-id',
-          component: () => import(/*webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage'),
-          props:(route) => {
-            const id = Number(route.params.id)
-            return isNaN( id ) ? { id: 1 } : { id }
-          },
-        },
-        {
-          path:'',
-          redirect: { name:'pokemon-about' }
-        }
-      ]
-    },
-    { 
-      path: '/:pathMach(.*)*', 
-      component:  () => import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')  
-    }
+      },
+      {
+        path: '',
+        redirect: { name: 'pokemon-about' }
+      }
+    ]
+  },
+  {
+    path: '/dbz',
+    name: 'DBZ',
+    component: () => import(/*webpackChunkName: "DBZLayout"*/'../modules/dbz/layouts/DragonBallLayout'),
+    children: [
+      {
+        path: 'characters',
+        name: 'dbz-characters',
+        component: () => import(/*webpackChunkName: "DBZCharacters"*/'../modules/dbz/pages/Characters')
+      },
+      {
+        path: 'about',
+        name: 'dbz-about',
+        component: () => import(/*webpackChunkName: "DBZAbout"*/'../modules/dbz/pages/About')
+      },
+      {
+        path: '',
+        redirect: { name: 'dbz-characters' }
+      }
+    ]
+  },
+  {
+    path: '/:pathMach(.*)*',
+    component: () => import(/*webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound')
+  }
 ]
 
 const router = createRouter({
